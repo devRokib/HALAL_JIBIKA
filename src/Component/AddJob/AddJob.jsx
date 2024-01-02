75f545415bb0ceefed35fcd2ba9fb22bd39b9775
devRokib <rakibahmedra971162@gmail.com>
2024-01-02 21:58:13 +0600

    add job done

--- src/Component/AddJob/AddJob.jsx
+++ src/Component/AddJob/AddJob.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import './AddJob.css'
-function AddJob({ createJob }) {
+import axios from 'axios';
+function AddJob() {
     const [jobData, setJobData] = useState({
         title: '',
         companyName: '',
         description: '',
         position: '',
-        logo: null,
+        logo:'',
     });
 
     const handleChange = (event) => {
@@ -17,7 +18,9 @@ function AddJob({ createJob }) {
         event.preventDefault();
 
         try {
-            await createJob(jobData);
+            const response = await axios.post(` http://localhost:9000/jobs`,jobData)
+            setJobData('job added success' ,response.data)
+            
             setJobData({ ...jobData, logo: null });
         } catch (error) {
             console.error(error);
@@ -36,13 +39,13 @@ function AddJob({ createJob }) {
                         <input type="text" id="companyName" name="companyName" value={jobData.companyName} onChange={handleChange} required />
 
                         <label htmlFor="description">Description:</label>
-                        <textarea id="description" name="description" value={jobData.description} onChange={handleChange} required />
+                        <input type='text' id="description" name="description" value={jobData.description} onChange={handleChange} required />
 
                         <label htmlFor="position">Position:</label>
-                        <input type="text" id="position" name="position" value={jobData.position} onChange={handleChange} required />
+                        <input type="text" id="position" name="position" value={jobData.position} onChange={handleChange}  />
 
-                        <label htmlFor="logo">Logo:</label>
-                        <input type="file" id="logo" name="logo" onChange={(event) => setJobData({ ...jobData, logo: event.target.files[0] })} /> <br />
+                        <label htmlFor="logo">Logo Url</label>
+                        <input type="text" id="logo" name="logo" value={jobData.logo} onChange={handleChange} /> <br />
 
                         <button type="submit">Add Job</button>
                     </form>
--- src/Layout/Footer/Footer.jsx
+++ src/Layout/Footer/Footer.jsx
@@ -1,26 +1,43 @@
+
+import { NavLink } from 'react-router-dom';
 import './Footer.css';
 
 function Footer() {
   return (
-    <footer>
+    <footer className='footerSection'>
       <div className="footer">
-      <div className="footerColumn">
-        <h3>Column 1</h3>
-        <p>Content for column 1 goes here.</p>
+        <div className="footerColumn">
+          <h3>HALAL JIBIKA</h3>
+          
+          <ul className='footerMenu'>
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to='/about'>About</NavLink>
+            <NavLink to='/jobs'>Jobs</NavLink>
+            <NavLink to='/jobs/:id/favorite'>Favorite</NavLink>
+            <NavLink to='/contact'>Contact</NavLink>
+          </ul>
+        </div>
+        <div className="footerColumn">
+          <h3>Column 2</h3>
+          <p>Content for column 2 goes here.</p>
+          <p>Additional content for column 2.</p>
+        </div>
+        <div className="footerColumn">
+          <h3>Column 3</h3>
+          <p>Content for column 3 goes here.</p>
+          <div>
+            <p>Contact Information:</p>
+            <p>Email: example@example.com</p>
+            <p>Phone: +1 (123) 456-7890</p>
+          </div>
+        </div>
       </div>
-      <div className="footerColumn">
-        <h3>Column 2</h3>
-        <p>Content for column 2 goes here.</p>
+      <hr style={{color:'#fff'}} />
+      <div className="footerBottom">
+        <p style={{textAlign:'center',color:'#fff', padding:'10px 0'}}>&copy; 2024 Your Website Name. All rights reserved.</p>
       </div>
-      <div className="footerColumn">
-        <h3>Column 3</h3>
-        <p>Content for column 3 goes here.</p>
-      </div>
-      </div>
-      <div className="footerBottom"></div>
     </footer>
   );
 }
 
 export default Footer;
-
